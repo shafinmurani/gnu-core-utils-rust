d6b77fd053ea144fe002650fe05aaf8968edeacc
Shafin Murani <shafinmurani9@gmail.com>
2024-04-08 10:31:35 +0000

    Added rm utility

---
+++ rm/src/lib.rs
@@ -0,0 +1,44 @@
+use std::path::Path;
+use std::{fs, io};
+
+pub struct Config<'a> {
+    pub files: &'a Vec<String>,
+}
+
+impl Config<'_> {
+    pub fn new(args: &Vec<String>) -> Config {
+        Config { files: args }
+    }
+
+    fn not_enough_arguments() {
+        eprintln!("rm: missing operand");
+        eprintln!("For help use: rm --help");
+    }
+
+    fn help() {
+        eprintln!("This is a cheap little clone of the rm utility in the GNU core utilities, the catch is that I made it in rust, check out more of my work at medium: https://shafinmurani.medium.com");
+        eprintln!("This is recursive by default so dont worry about anything, just run it :D");
+        eprintln!("To use this util: rm path");
+    }
+
+    fn rm_dir_all(src: impl AsRef<Path>) -> io::Result<()> {
+        fs::remove_dir_all(src)?;
+        Ok(())
+    }
+
+    pub fn run(&self) {
+        if self.files.len() == 0 {
+            Self::not_enough_arguments();
+        } else if self.files.contains(&String::from("--help")) {
+            Self::help();
+        } else {
+            for entry in self.files {
+                let result = Self::rm_dir_all(entry);
+                match result {
+                    Ok(()) => {}
+                    Err(e) => eprintln!("Application Error: {}", e),
+                };
+            }
+        }
+    }
+}
---
+++ rm/src/main.rs
@@ -0,0 +1,9 @@
+use rm::Config;
+use std::env;
+
+fn main() {
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
+    let config = Config::new(&args);
+    config.run();
+}
